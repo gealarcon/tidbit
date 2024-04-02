19aea77a9248dd7ee4af05ebeea0a4b4f4154906
Geovanni Alarcon <geovanni.alarcon@neochromosome.com>
2024-04-01 19:45:15 -0700

    init router and pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,15 @@
+import Head from "next/head";
+import Link from "next/link";
+
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div>
-      
+      <Link href={{
+        pathname: "/[username]",
+        query: {username: "geo23"}
+      }}>Profile</Link>
     </div>
   )
 }
